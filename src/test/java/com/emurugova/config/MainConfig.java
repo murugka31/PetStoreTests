23040a1f1807f9c2c24a8f67ac78eca2a7245dfb
Murugova Elena <118747260+murugka31@users.noreply.github.com>
2023-05-01 11:52:40 +0300

    Delete MainConfig.java

--- src/test/java/com/emurugova/config/MainConfig.java
+++
@@ -1,9 +0,0 @@
-package com.emurugova.config;
-
-import org.aeonbits.owner.Config;
-
-@Config.Sources("classpath:config/credentials.properties")
-public interface MainConfig extends Config {
-
-    String hostname();
-}
