862dda6be0e73715690678b5203989bedd30b6a7
murugka31 <murugka31@mail.ru>
2023-05-01 11:44:25 +0300

    Add Faker library

--- src/test/java/com/emurugova/tests/TestData.java
+++ src/test/java/com/emurugova/tests/TestData.java
@@ -1,34 +1,38 @@
 package com.emurugova.tests;
 
+import com.github.javafaker.Faker;
+
 public class TestData {
 
-    public static int petId = 1111;
-    public static int noPetId = 111100000;
-    public static int categoryId = 1;
-    public static String categoryName = "dog";
-    public static String petName = "Loki";
+    public static Faker faker = new Faker();
+
+    public static int petId = faker.number().numberBetween(1110,1200);
+    public static int noPetId = faker.number().numberBetween(2000,3000);;
+    public static int categoryId = faker.number().numberBetween(1,5);
+    public static String categoryName = faker.animal().name();
+    public static String petName = faker.pokemon().name();
     public static String petPhotoUrls = "https://zooclub.ru/attach/10000/10091.jpg";
-    public static int petTagsId = 1;
-    public static String petTagsName = "no name";
-    public static String petStatus = "available";
+    public static int petTagsId = faker.number().numberBetween(1,3);;
+    public static String petTagsName = faker.lordOfTheRings().character();
+    public static String petStatus =faker.dog().breed();
     public static String newPetData = "{ \"id\": "+petId+", \"category\": { \"id\": "+categoryId+", \"name\": \""+categoryName+"\" }, " +
             "\"name\": \""+petName+"\", \"photoUrls\": [ \""+petPhotoUrls+"\" ], \"tags\": [ { \"id\": "+petTagsId+", " +
             "\"name\": \""+petTagsName+"\" } ], \"status\": \""+petStatus+"\" }";
 
-    public static int purchaseOrder = 100;
-    public static int noPurchaseOrder = 3;
+    public static int purchaseOrder = faker.number().numberBetween(100,120);
+    public static int noPurchaseOrder = faker.number().numberBetween(1000,1200);;
     public static String newPurchaseOrderData = "{ \"id\": "+purchaseOrder+", \"petId\": "+petId+", \"quantity\": 0, " +
             "\"shipDate\": \"2023-04-16T07:42:39.059Z\", \"status\": \"placed\", \"complete\": true }";
 
-    public static int userId = 1114;
-    public static String userName = "UserJohny";
-    public static String noUserName = "UserJohny2";
-    public static String userFirstName = "John";
-    public static String userLastName = "Johnas";
-    public static String userEmail = "mail@mail.ru";
-    public static String userPassword = "12345";
-    public static String userPhone = "89999999999";
-    public static int userStatus = 1;
+    public static int userId = faker.number().numberBetween(1000,1200);
+    public static String userName = faker.name().firstName();
+    public static String noUserName = faker.harryPotter().spell();
+    public static String userFirstName = faker.name().firstName();
+    public static String userLastName = faker.name().lastName();
+    public static String userEmail = faker.internet().emailAddress();
+    public static String userPassword = faker.harryPotter().house();
+    public static String userPhone = faker.phoneNumber().phoneNumber();
+    public static int userStatus = faker.number().numberBetween(1,5);;
     public static String newUserData = "{ \"id\": "+userId+", \"username\": \""+userName+"\", \"firstName\": \""+userFirstName+"\", " +
             "\"lastName\": \""+userLastName+"\", \"email\": \""+userEmail+"\", \"password\": \""+userPassword+"\", \"phone\": \""+userPhone+"\", \"userStatus\": "+userStatus+"}";
 }
